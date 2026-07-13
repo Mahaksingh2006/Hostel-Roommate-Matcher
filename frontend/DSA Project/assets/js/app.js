41ea475be26f8bd0d72add6164526d177ff0cd6c
Mahak Singh <mehaksinghkp@gmail.com>
2026-07-13 12:57:54 +0530

    Connect frontent to deployed backend

--- frontend/DSA Project/assets/js/app.js
+++ frontend/DSA Project/assets/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://hostel-roommate-matcher.onrender.com";
 const students = new Map();
 let latestRecommendations = [];
 let recentActivity = [];
